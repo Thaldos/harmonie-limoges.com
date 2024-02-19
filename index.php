55d1be243fce4a420160e5a88e08329b74514e21
Vincent Godé <vincent.gode@legrand.com>
2024-02-19 11:32:04 +0100

    Create waiting index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Harmonie Limoges</title>
+	<link rel="stylesheet" href="css/harmonie-limoges.css">
+</head>
+<body>
+    <div class="wrapper">
+		<div class="circle"></div>
+		<div class="circle"></div>
+		<div class="circle"></div>
+		<div class="shadow"></div>
+		<div class="shadow"></div>
+		<div class="shadow"></div>
+	</div>
+</body>
+</html>
